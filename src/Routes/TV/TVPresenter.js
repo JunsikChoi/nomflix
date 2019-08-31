8ed9adb1d663019fc58621ec9c84c5d231c2c1c0
Jun-Sik Choi <leichoi91@gmail.com>
2019-08-31 21:50:31 +0900

    Set presenter structure

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,15 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPresenter = (topRated, popular, airingToday, loading, error) => null;
+
+TVPresenter.PropTypes = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airingToday: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired
+};
+
+export default TVPresenter;
