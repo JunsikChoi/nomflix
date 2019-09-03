bf4b5f8d98f00229907fa911b7d88c273dc0ddbd
Jun-Sik Choi <leichoi91@gmail.com>
2019-09-03 23:19:14 +0900

    Add Poster to everywhere / React Helmet

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,43 +1,75 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Section from "Components/Section";
 import Loader from "../../Components/Loader";
-import Error from "../../Components/Error";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 20px;
 `;
 
-const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
-  loading ? (
-    <Loader />
-  ) : (
-    <Container>
-      {nowPlaying && nowPlaying.length > 0 && (
-        <Section title="Now Playing">
-          {nowPlaying.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
-          ))}
-        </Section>
-      )}
-      {upcoming && upcoming.length > 0 && (
-        <Section title="Upcoming">
-          {upcoming.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular">
-          {popular.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
-          ))}
-        </Section>
-      )}
-      {error && <Error text={error}></Error>}
-    </Container>
-  );
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) => (
+  <>
+    <Helmet>
+      <title>Movies | Nomflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {nowPlaying && nowPlaying.length > 0 && (
+          <Section title="Now Playing">
+            {nowPlaying.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                title={movie.original_title}
+                imageUrl={movie.poster_path}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {upcoming && upcoming.length > 0 && (
+          <Section title="Upcoming">
+            {upcoming.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                title={movie.original_title}
+                imageUrl={movie.poster_path}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular">
+            {popular.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                title={movie.original_title}
+                imageUrl={movie.poster_path}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message text={error} color="#e74c3c" />}
+      </Container>
+    )}
+  </>
+);
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,43 +1,75 @@
 import React from "react";
 import propTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
-import Error from "../../Components/Error";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 20px;
 `;
 
-const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  loading ? (
-    <Loader />
-  ) : (
-    <Container>
-      {topRated && topRated.length > 0 && (
-        <Section title="Top Rated Shows">
-          {topRated.map(show => (
-            <span id={show.id}>{show.name}</span>
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Shows">
-          {popular.map(show => (
-            <span id={show.id}>{show.name}</span>
-          ))}
-        </Section>
-      )}
-      {airingToday && airingToday.length > 0 && (
-        <Section title="Airing Today Shows">
-          {airingToday.map(show => (
-            <span id={show.id}>{show.name}</span>
-          ))}
-        </Section>
-      )}
-      {error && <Error text={error}></Error>}
-    </Container>
-  );
+const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => (
+  <>
+    <Helmet>
+      <title>TV Shows | Nomflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {topRated && topRated.length > 0 && (
+          <Section title="Top Rated Shows">
+            {topRated.map(show => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                title={show.original_name}
+                imageUrl={show.poster_path}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+                isMovie={false}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Shows">
+            {popular.map(show => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                title={show.original_name}
+                imageUrl={show.poster_path}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+                isMovie={false}
+              />
+            ))}
+          </Section>
+        )}
+        {airingToday && airingToday.length > 0 && (
+          <Section title="Airing Today Shows">
+            {airingToday.map(show => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                title={show.original_name}
+                imageUrl={show.poster_path}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+                isMovie={false}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message text={error} color="#e74c3c" />}
+      </Container>
+    )}
+  </>
+);
 
 TVPresenter.propTypes = {
   topRated: propTypes.array,
