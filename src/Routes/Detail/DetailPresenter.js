9b090978f4d04045705f49a7f653fe59fe6a3e38
Jun-Sik Choi <leichoi91@gmail.com>
2019-09-03 23:32:27 +0900

    Add error handling in Detail Presenter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Helmet from "react-helmet";
 import Loader from "../../Components/Loader";
+import Message from "../../Components/Message";
 
 const Container = styled.div`
   height: calc(100vh - 50px);
@@ -76,6 +77,8 @@ const DetailPresenter = ({ result, loading, error }) =>
       </Helmet>
       <Loader />
     </>
+  ) : error ? (
+    <Message color="red" text="Can't find detail information." />
   ) : (
     <Container>
       <Helmet>
